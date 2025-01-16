73463cfe2501cda1da879b867ece8d057e3c000d
samork26 <vohra207@gmail.com>
2025-01-16 18:33:38 -0500

    Initial Source Code

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './pages/**/*.html',  // HTML files inside the pages directory
+    './styles/**/*.css',  // Any additional CSS files
+    './scripts/**/*.js',  // JavaScript files in the scripts directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
