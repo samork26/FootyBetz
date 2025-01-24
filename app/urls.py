b887bcca4a17e8b13281adf797b18dce4b884f5c
samork26 <vohra207@gmail.com>
2025-01-24 18:24:22 -0500

    Added sample route to about page

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # Example view
+    path('about/', views.about, name='about')
 ]
--- app/views.py
+++ app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')  # Pointing to templates/home.html
+
+def about(request):
+    return render(request, 'about.html')
